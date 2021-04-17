8ceebaba94834490239ccd437ef26743727a31a7
Sim_Kim_Wee <sim.kim.wee.012s@gmail.com>
2021-04-17 13:58:07 +0800

    Initial Commit, no backbone implemented yet.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './pages/main/dashboard/dashboard.component';
+import { MainComponent } from './pages/main/main.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { 
+    path: "v1",
+    component: MainComponent,
+    children: [
+      {
+        path: "dashboard",
+        component: DashboardComponent
+      },
+      { path: "**", redirectTo: "/v1" },
+      { path: " ", redirectTo: "/v1" }
+    ]
+  },
+  /*
+  .
+  .
+  .
+  .
+  .
+  .
+  */
+  { path: "**", redirectTo: "/v1" },
+  { path: " ", redirectTo: "/v1" },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigatorComponent } from './layout/navigator/navigator.component';
+import { NoticeboardComponent } from './layout/noticeboard/noticeboard.component';
+import { MainComponent } from './pages/main/main.component';
+import { LoginComponent } from './pages/login/login.component';
+import { DashboardComponent } from './pages/main/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigatorComponent,
+    NoticeboardComponent,
+    MainComponent,
+    LoginComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
